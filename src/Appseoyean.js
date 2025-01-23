8cd6746723f56d3251b6e233030b7b25e1309fc8
yean1234 <peaseoyean@gmail.com>
2025-01-23 14:37:39 +0900

    파일 이름 수정

--- src/Appseoyean.js
+++ src/Appseoyean.js
@@ -1,6 +1,6 @@
 import Banner from "./components/Banner";
 
-function App() {
+function Appseoyean() {
   return (
     <div>
       <Banner />
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default Appseoyean;
